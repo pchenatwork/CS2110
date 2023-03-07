0e86d33adc2a0c20a0c31d35328e19ae83ff02be
Paul Chen <pchenatwork@hotmail.com>
2023-03-07 17:54:41 -0500

    Add "AX" as new Java Project template (with launch.json / args[] setting)

---
+++ assignments/AX/src/cs2110/LinkedSeq.java
@@ -0,0 +1,288 @@
+package cs2110;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/*
+ * Assignment metadata
+ * Name(s) and NetID(s): Stanley Chen (syc33)
+ * Hours spent on assignment: 10
+ */
+
+/**
+ * A list of elements of type `T` implemented as a singly linked list.  Null elements are not
+ * allowed.
+ */
+public class LinkedSeq<T> implements Seq<T> {
+
+    /**
+     * Number of elements in the list.  Equal to the number of linked nodes reachable from `head`.
+     */
+    private int size;
+
+    /**
+     * First node of the linked list (null if list is empty).
+     */
+    private Node<T> head;
+
+    /**
+     * Last node of the linked list starting at `head` (null if list is empty).  Next node must be
+     * null.
+     */
+    private Node<T> tail;
+
+    /**
+     * Assert that this object satisfies its class invariants.
+     */
+    private void assertInv() {
+        assert size >= 0;
+        if (size == 0) {
+            assert head == null;
+            assert tail == null;
+        } else {
+            assert head != null;
+            assert tail != null;
+
+            // Check that the number of linked nodes is equal to this list's size
+            int count = 0;
+            Node<T> curr = head;
+            while (curr != null) {
+                count++;
+                curr = curr.next();
+            }
+            assert count == size;
+
+            // Check that the last linked node is the same object as `tail`.
+            curr = head;
+            while (curr.next() != null) {
+                curr = curr.next();
+            }
+            assert curr == tail;
+        }
+    }
+
+    /**
+     * Create an empty list.
+     */
+    public LinkedSeq() {
+        size = 0;
+        head = null;
+        tail = null;
+
+        assertInv();
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void prepend(T elem) {
+        assertInv();
+        assert elem != null;
+
+        head = new Node<>(elem, head);
+        // If list was empty, assign tail as well
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+
+        assertInv();
+    }
+
+    /**
+     * Return a text representation of this list with the following format: the string starts with
+     * '[' and ends with ']'.  In between are the string representations of each element, in
+     * sequence order, separated by ", ".
+     * <p>
+     * Example: a list containing 4 7 8 in that order would be represented by "[4, 7, 8]".
+     * <p>
+     * Example: a list containing two empty strings would be represented by "[, ]".
+     * <p>
+     * The string representations of elements may contain the characters '[', ',', and ']'; these
+     * are not treated specially.
+     */
+    @Override
+    public String toString() {
+        String str = "[";
+        Node<T> curr = head;
+        while (curr != null) {
+            str += curr.data();
+            curr = curr.next();
+            if (curr != null) {
+                str += ", ";
+            }
+        }
+        str += "]";
+        return str;
+    }
+
+    @Override
+    public boolean contains(T elem) {
+        Node<T> curr = head;
+        while (curr != null) {
+            if (curr.data().equals(elem)) {
+                return true;
+            }
+            curr = curr.next();
+        }
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        assertInv();
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        Node<T> curr = head;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next();
+        }
+        assertInv();
+        return curr.data();
+    }
+
+    @Override
+    public void append(T elem) {
+        assert elem != null;
+        Node<T> successor = new Node<>(elem, null);
+        if (head == null) {
+            head = successor;
+        } else {
+            Node<T> curr = head;
+            while (curr.next() != null) {
+                curr = curr.next();
+            }
+            curr.setNext(successor);
+            tail = successor;
+        }
+        size++;
+    }
+
+    @Override
+    public void insertBefore(T elem, T successor) {
+        if (!contains(successor))
+            throw new IllegalArgumentException("Successor not found in list: " + successor);
+        if (elem == null)
+            throw new IllegalArgumentException ("'elem' can not be null.");
+        Node<T> curr = head;
+        Node<T> prev = null;
+        while (curr!=null) {
+            if (curr.data().equals(successor)){
+                Node<T> newNode = new Node<T>(elem, curr);
+                if (prev == null) {
+                    head = newNode;
+                }
+                else {
+                    prev.setNext(newNode);
+                }
+                size++;
+                return;
+            }
+            prev = curr;
+            curr = curr.next();
+        }
+    }
+
+    @Override
+    public boolean remove(T elem) {
+        Node<T> prev = null;
+        Node<T> curr = head;
+        int removed = 0;
+        while (curr!=null) {
+            if (curr.data().equals(elem)){
+                if (curr == head || curr ==tail) {
+                    head = (curr == head) ? head.next() : head;
+                    if (curr == tail) {
+                        tail = prev;
+                        if (prev!=null) {
+                            tail.setNext(null);
+                        }
+                    }
+                } else {
+                    prev.setNext(curr.next());
+                }
+                removed ++;
+                size--;
+            }
+            prev = curr;
+            curr = curr.next();
+        }
+        return removed > 0;
+    }
+
+    /**
+     * Return whether this and `other` are `LinkedSeq`s containing the same elements in the same
+     * order.  Two elements `e1` and `e2` are "the same" if `e1.equals(e2)`.  Note that `LinkedSeq`
+     * is mutable, so equivalence between two objects may change over time.  See `Object.equals()`
+     * for additional guarantees.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof LinkedSeq)) {
+            return false;
+        }
+        LinkedSeq otherSeq = (LinkedSeq) other;
+        Node<T> currNodeThis = head;
+        Node currNodeOther = otherSeq.head;
+
+        while (currNodeThis != null && currNodeOther != null) {
+            if (!currNodeThis.data().equals(currNodeOther.data())) {
+                return false;
+            }
+            currNodeThis = currNodeThis.next();
+            currNodeOther = currNodeOther.next();
+        }
+        return true;
+    }
+
+    /*
+     * There is no need to read the remainder of this file for the purpose of completing the
+     * assignment.  We have not yet covered the implementation of these concepts in class.
+     */
+
+    /**
+     * Returns a hash code value for the object.  See `Object.hashCode()` for additional
+     * guarantees.
+     */
+    @Override
+    public int hashCode() {
+        // Whenever overriding `equals()`, must also override `hashCode()` to be consistent.
+        // This hash recipe is recommended in _Effective Java_ (Joshua Bloch, 2008).
+        int hash = 1;
+        for (T e : this) {
+            hash = 31 * hash + e.hashCode();
+        }
+        return hash;
+    }
+
+    /**
+     * Return an iterator over the elements of this list (in sequence order).  By implementing
+     * `Iterable`, clients can use Java's "enhanced for-loops" to iterate over the elements of the
+     * list.  Requires that the list not be mutated while the iterator is in use.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        assertInv();
+
+        // Return an instance of an anonymous inner class implementing the Iterator interface.
+        // For convenience, this uses Java features that have not eyt been introduced in the course.
+        return new Iterator<>() {
+            private Node<T> next = head;
+            public T next() throws NoSuchElementException {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T result = next.data();
+                next = next.next();
+                return result;
+            }
+            public boolean hasNext() {
+                return next != null;
+            }
+        };
+    }
+}
---
+++ assignments/AX/src/cs2110/Node.java
@@ -0,0 +1,55 @@
+package cs2110;
+
+/**
+ * A node of a singly linked list whose elements have type `T`.
+ */
+public class Node<T> {
+
+    /**
+     * The list element in this node.
+     */
+   // private final T data;
+    private T data;  ///*** ###PCHEN### demo share linkedSeq() ****/
+
+    /**
+     * Next node on list (null if this is the last node).
+     */
+    private Node<T> next;
+
+    /**
+     * Create a Node containing element `elem` and pointing to successor node `successor` (may be
+     * null).
+     */
+    public Node(T elem, Node<T> successor) {
+        data = elem;
+        next = successor;
+    }
+
+    /**
+     * Return the list element contained in this node.
+     */
+    public T data() {
+        return data;
+    }
+    /**
+     *  ###PCHEN### add a 'setter' to demo Shared linkSeq
+     *  ###Explain overloading ###
+     *  */
+    public void data(T elem) {
+        data = elem;
+    }
+
+    /**
+     * Return the node after this one in the list (null if this is the last node of the list).
+     */
+    Node<T> next() {
+        return next;
+    }
+
+    /**
+     * Effect: make `successor` the successor to this node (`successor` may be null).
+     */
+    void setNext(Node<T> successor) {
+        next = successor;
+    }
+}
